8a5bd55e7ae3fcdf80adb584711f877699e5900c
Baldev Sahu <bsahu@codeaurora.org>
2021-02-19 15:53:22 +0530

    hwc2 : Add scaling rgb layer flag to layer stack
    Add scaling rgb layer flag to layer stack. This flag is
    used to avoid idle fallback which causes flicker due
    to mdp vs gpu output difference.
    
    Change-Id: I9318e407fd1b56359c2f98de0d62784ee9803ed1

--- sdm/libs/hwc2/hwc_display.cpp
+++ sdm/libs/hwc2/hwc_display.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2021, The Linux Foundation. All rights reserved.
  * Not a Contribution.
  *
  * Copyright 2015 The Android Open Source Project
@@ -701,6 +701,10 @@ void HWCDisplay::BuildLayerStack() {
 
     if (layer->input_buffer.flags.secure_display) {
       is_secure = true;
+    }
+
+    if (IS_RGB_FORMAT(layer->input_buffer.format) && hwc_layer->IsScalingPresent()) {
+      layer_stack_.flags.scaling_rgb_layer_present = true;
     }
 
     if (hwc_layer->IsSingleBuffered() &&
